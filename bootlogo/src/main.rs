37346911c620431036c05e80a9d32d0f52d3cf92
cobaltgit <cobaltsecuremail312@proton.me>
2025-11-14 21:18:09 +0000

    bootlogo: refactorings

--- bootlogo/src/main.rs
+++ bootlogo/src/main.rs
@@ -3,7 +3,13 @@ use std::io::{self, Read, Write};
 use std::env::args;
 use std::process::exit;
 
-fn validate_bmp(path: &str) -> Result<bool, std::io::Error> {
+const REQUIRED_WIDTH: i32 = 240
+const REQUIRED_HEIGHT: i32 = 320
+const MAX_BOOTLOGO_SIZE: u32 = 524288 // size of /dev/mmcblk0p2, partition holding the bootlogo
+const HEADER_SIZE: usize = 54
+const BMP_SIZE: usize = 153738 // max theoretical size of 240x320 RGB565 bitmap with GIMP V5 header
+
+fn validate_bmp(path: &str) -> io::Result<()> {
     let meta = metadata(&path)?;
 
     if !meta.is_file() {
@@ -11,13 +17,13 @@ fn validate_bmp(path: &str) -> Result<bool, std::io::Error> {
         exit(1);
     }
 
-    if meta.len() > 524288 {
+    if meta.len() > MAX_BOOTLOGO_SIZE {
         eprintln!("Bootlogo must be under 512KiB!");
         exit(1);
     }
     
     let mut file = File::open(path)?;
-    let mut header = [0u8; 54];
+    let mut header = [0u8; HEADER_SIZE];
     file.read_exact(&mut header)?;
 
     if &header[0..2] != b"BM" {
@@ -33,18 +39,20 @@ fn validate_bmp(path: &str) -> Result<bool, std::io::Error> {
     if bits_per_pixel != 16 || compression != 3 {
         eprintln!("Bootlogo must be 16-bit RGB565 format!");
         exit(1);
-    } else if (width, height.abs()) != (240, 320) {
+    }
+
+    if (width, height.abs()) != (REQUIRED_WIDTH, REQUIRED_HEIGHT) {
         eprintln!("Bootlogo must be 240x320 resolution!");
         exit(1);
-    } else {
-        Ok(true)
     }
+
+    Ok(())
 }
 
 fn write_bootlogo(path: &str) -> io::Result<()> {
     let mut bmp = File::open(&path)?;
-    let mut bootlogo_data = Vec::new();
-    bmp.read_to_end(&mut bootlogo_data)?;
+    let mut bootlogo_data = [0u8; BMP_SIZE];
+    bmp.read(&mut bootlogo_data)?;
     
     let mut file = OpenOptions::new()
         .write(true)
@@ -62,21 +70,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .map(String::as_str)
         .unwrap_or_else(|| "bootlogo.bmp");
 
-    match validate_bmp(&bootlogo_path) {
-        Err(e) => {
-            eprintln!("Error validating bootlogo: {}", e);
-            exit(1);
-        }
-        _ => {}
-    };
-    match write_bootlogo(&bootlogo_path) {
-        Ok(()) => {
-            println!("Bootlogo written successfully!");
-            Ok(())
-        }
-        Err(e) => {
-            eprintln!("Error writing bootlogo: {}", e);
-            exit(1);
-        }
+    if let Err(e) = validate_bmp(&bootlogo_path) {
+        eprintln!("Error validating bootlogo: {}", e);
+        exit(1);
     }
+    
+    if let Err(e) = write_bootlogo(&bootlogo_path) {
+        eprintln!("Error writing bootlogo: {}", e);
+        exit(1);
+    }
+
+    Ok(())
 }
